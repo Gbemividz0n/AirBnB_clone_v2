36f80c2c71e2af17f3a57214bed721773095fa80
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2022-09-27 23:24:00 +0100

    Amenity_

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+'''
+    Implementation of the Amenity class
+'''
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+import os
 from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    '''
+        Implementation for the Amenities.
+    '''
+    __tablename__ = 'amenities'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False, default="")
+        place_amenities = relationship(
+            "Place", secondary=place_amenity)
+    else:
+        name = ""
