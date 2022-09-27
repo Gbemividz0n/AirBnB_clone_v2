2b3d49ef9ade34ef5c16a8aeacedb9b81d5d99ae
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2022-09-27 23:26:45 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
+'''
+    Define the class City.
+'''
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+from sqlalchemy import String, Column, ForeignKey
 from sqlalchemy.orm import relationship
-from models.place import Place
+import os
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
+    __tablename__ = 'cities'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", passive_deletes=True, backref="cities")
+    else:
+        state_id = ""
+        name = ""
